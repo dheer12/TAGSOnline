d6d82a15fb7f9a44589c85f6943d6c16e4349e56
dheeraj maddini <dheer12@gmail.com>
2018-01-16 22:54:11 -0800

    Admin user show buttons

---
+++ TAGS_BARCODE_WEBAPP_V4/Javascript/HomeController.js
@@ -0,0 +1,11 @@
+﻿var App = angular.module('App', []);
+
+App.controller('HomeCrtl', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.IsUserAdmin = false;
+
+    $http.get("api/IsUserAdmin")
+        .then(function (response) {
+            $scope.IsUserAdmin = response.data;
+        });
+}]);
